4be6ecf894d27c27a91df624f02f075f8fc9c28b
Artur Gawryszczak <gawrysz@gmail.com>
2026-01-24 19:25:48 +0100

    [codee] coments on last two, hard to remove, warnings

--- src/IO/hdf5/data_hdf5.F90
+++ src/IO/hdf5/data_hdf5.F90
@@ -704,6 +704,7 @@ contains
       end associate
 
    end subroutine datafields_hdf5
+! codee doesn't like inserting conditional parts into select statement
 
 !
 ! ------------------------------------------------------------------------------------
